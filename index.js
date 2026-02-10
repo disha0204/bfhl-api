cd458a3512c1f43caa4e8621233e1d6f74dd28c3
unknown <disha0327.be23@chitkara.edu.in>
2026-02-10 12:32:22 +0530

    routes fix

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.use(express.json());
 
 const EMAIL = "disha0327.be23@chitkara.edu.in";
 
+app.get("/", (req, res) => {
+  res.send("Server Working");
+});
+
 app.get("/health", (req, res) => {
   res.json({
     is_success: true,
@@ -16,12 +20,12 @@ app.post("/bfhl", (req, res) => {
   res.json({
     is_success: true,
     official_email: EMAIL,
-    data: "working"
+    data: "API Working"
   });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log("Running on", PORT);
 });
